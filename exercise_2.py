644c219c9db0489cc54b965b35896af2fd04a802
Jackson Vo <jacksonvo@ucla.edu>
2015-03-18 05:09:17 +0000

    Moves corresponding files to respective directories.

---
+++ exercise_2.py
@@ -0,0 +1,4 @@
+mv shell_fun/*.inn inn #moves all files contaning .inn to inn directory
+mv shell_fun/*.nex nex #moves all files contaning .nex to nex directory
+mv shell_fun/*.parm parm #moves all files contaning .parm to parm directory
+mv shell_fun/*.tree tree #moves all files contaning .tree to tree directory
